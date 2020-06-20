c44f43e76cb22f60288d206a4de03cb498bdeb75
aathya <aadithya8111@gmail.com>
2020-06-20 15:37:55 +0530

    fixes

--- server_control.rb
+++ server_control.rb
@@ -1,3 +1,16 @@
 require 'daemons'
 
-Daemons.run('server.rb')
+pwd  = File.dirname(File.expand_path(__FILE__))
+file = pwd + '/server.rb'
+
+Daemons.run_proc(
+    'action-tunnel', # name of daemon
+    #  :dir_mode => :normal
+     :dir => File.join(pwd, '/tmp/server/pids'), # directory where pid file will be stored
+    #  :backtrace => true,
+    #  :monitor => true,
+    :log_output => true
+) do
+  exec "ruby #{file}"
+end
+
