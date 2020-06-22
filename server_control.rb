2719e72ae4c6df395e6859f58acc2a6005bafff7
aathya <aadithya8111@gmail.com>
2020-06-22 18:53:20 +0530

    fixes

--- server_control.rb
+++ server_control.rb
@@ -3,7 +3,7 @@ require 'daemons'
 pwd  = File.dirname(File.expand_path(__FILE__))
 file = pwd + '/server.rb'
 
-Daemons.run_proc('action-tunnel', no_pidfiles: true, multiple: true) do
+Daemons.run_proc('action-tunnel', no_pidfiles: true, log_output: true, log_dir: "#{pwd}/tmp") do
   exec "ruby #{file}"
 end
 
